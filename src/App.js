c2db20a19d648a8dd1ec4863a3de57baa2e06630
Thewessen <sthewessen@gmail.com>
2019-05-27 23:07:10 +0200

    First idea of a singlepage webapp created by React, using it's modular component creation and rendering.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Genres from './Genres.js'
+import Albums from './Albums.js'
 
 function App() {
   return (
-    <div className="App">
+    <main className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Beets Genre Tagger</h1>
       </header>
-    </div>
+      <div className="App-container">
+        <div className="Genres">
+          <Genres></Genres>
+        </div>
+        <div className="Albums">
+          <Albums></Albums>
+        </div>
+      </div>
+    </main>
   );
 }
 
---
+++ src/Genres.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Genres.css';
+
+function Genres() {
+  return (
+    <div className="Genre-container">
+      <ul>
+        <li>Blues</li>
+        <li>Classical</li>
+        <li>Electronic</li>
+        <li>Jazz</li>
+        <li>Rap</li>
+        <li>R&B</li>
+        <li>Reggae</li>
+        <li>Rock</li>
+        <li>Vocal</li>
+        <li>World</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Genres;
