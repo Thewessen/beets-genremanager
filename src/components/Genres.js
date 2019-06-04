211b39ca32172520c00828fa14cfc86c71b08f9e
Bart Kleijngeld <bartkl@gmail.com>
2019-06-04 17:13:11 +0200

    removed useEffect / fetch from Genres

--- src/components/Genres.js
+++ src/components/Genres.js
@@ -1,16 +1,8 @@
-import React, { useEffect } from 'react'
 import Genre from './Genre.js'
 import { connect } from 'react-redux'
 import '../Genres.css'
 
 const Genres = (props) => {
-  useEffect(() => { /* I would however prefer to not use effects,
-                       but to simply perform this outside the Component,
-                       and to pass the data in via props or set it in state. */
-    fetch('https://randomuser.me/api/')
-      .then(function (data) { return data.json() })
-      .then(results => console.log(results.results[0]))
-  }, [])
   return (
     <div id="genres">
       <ul>
