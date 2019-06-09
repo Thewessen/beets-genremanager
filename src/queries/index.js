43d4fabb808289afb1701459d0d9280d43c1776d
Bart Kleijngeld <bartkl@gmail.com>
2019-06-09 19:59:56 +0200

    added album id to query and some css minor change

--- src/queries/index.js
+++ src/queries/index.js
@@ -3,6 +3,7 @@ import gql from 'graphql-tag'
 export const GET_ALBUMS = gql`
   query Albums($nameContains: String, $genreIn: [String]) {
     albums(nameContains: $nameContains, genreIn: $genreIn) {
+      id
       name
       albumArtist
       genre
